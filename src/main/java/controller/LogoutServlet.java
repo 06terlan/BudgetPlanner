820ec4b7008e33fde78f5a1055f0a323668d4453
kos602 <kalmurzaev.oljas@gmail.com>
2018-11-17 18:07:53 -0600

    delete views

--- src/main/java/controller/LogoutServlet.java
+++ src/main/java/controller/LogoutServlet.java
@@ -10,7 +10,6 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if(req.getSession(false) != null) req.getSession().invalidate();
-
         resp.sendRedirect("/");
     }
 }
